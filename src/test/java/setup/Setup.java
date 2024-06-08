4c46a24512e2a3adb45a4b42a205b8faebead15f
Mamun104 <muhammadmamunurrashid360@gmail.com>
2024-06-08 23:05:42 +0600

    upload project

--- src/test/java/setup/Setup.java
+++ src/test/java/setup/Setup.java
@@ -1,11 +1,16 @@
 package setup;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.TestInstance;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 public class Setup {
@@ -13,7 +18,19 @@ public class Setup {
     public WebDriver driver;
 
     @BeforeTest
-    public void setup() {
+    public void setup() throws MalformedURLException {
+
+        // Initiating the WebDriver
+        DesiredCapabilities dc = new DesiredCapabilities();
+
+        // Setting the browser as Chrome
+        dc.setBrowserName("chrome");
+
+        // Providing the URL for the docker Container
+        URL url=new URL("http://localhost:4444/wd/hub");
+
+        // Creating a webdriver
+        RemoteWebDriver remoteWebDriver=new RemoteWebDriver(url,dc);
 
         driver = new ChromeDriver();
         driver.manage().window().maximize();
